5af0d2892b59b9fa61418a6b1b85fcff5148f793
Dmitry <117797846+Dmitry-Markelov@users.noreply.github.com>
2023-10-06 19:51:02 +0400

    add working triangle

--- server/application/Triangle.php
+++ server/application/Triangle.php
@@ -1,10 +1,86 @@
 <?php
-
+//http://php-project/server/?method=triangle&Ax=-2&Ay=1&Az=2&Bx=3&By=-3&Bz=5&Cx=1&Cy=0&Cz=9
 class Triangle{
-    public function method1($a = 0, $b = 0) {
-        return $a + $b;
+    function __construct() {
+        
     }
-    public function method2($a = 0, $b = 0) {
-        return $a * $b;
+    public function triangle($params) {
+        $A['x'] = $params['Ax'];
+        $A['y'] = $params['Ay'];
+        $A['z'] = $params['Az'];
+        $B['x'] = $params['Bx'];
+        $B['y'] = $params['By'];
+        $B['z'] = $params['Bz'];
+        $C['x'] = $params['Cx'];
+        $C['y'] = $params['Cy'];
+        $C['z'] = $params['Cz'];
+        print_r($A);
+        print_r($B);
+        print_r($C);
+
+        return array(
+            self::square($A, $B, $C),
+            self::angle($A, $B, $C),
+            self::sidesLength($A, $B, $C)
+        );
     }
+    function square($A, $B, $C){ //площадь
+        $S = 0;
+        $AB = self::vector($A, $B);
+        $AC = self::vector($A, $C);
+        $vProd = self::vProd($AB, $AC);
+        $S = 1/2*self::vModul($vProd);
+        
+        return round($S, 1);
+    }
+    function angle($A, $B, $C){ //углы
+        $angles = array();
+    
+        $vAB = self::vector($A, $B);
+        $vAC = self::vector($A, $C);
+    
+        $vBA = self::vector($B, $A);
+        $vBC = self::vector($B, $C);
+    
+        $vCB = self::vector($C, $B);
+        $vCA = self::vector($C, $A);
+    
+        $angles['AB&AC'] = round(acos(self::sProd($vAB, $vAC)/(self::vModul($vAB)*self::vModul($vAC)))*180/pi(), 1);
+        $angles['BA&BC'] = round(acos(self::sProd($vBA, $vBC)/(self::vModul($vBA)*self::vModul($vBC)))*180/pi(), 1);
+        $angles['CB&CA'] = round(acos(self::sProd($vCB, $vCA)/(self::vModul($vCB)*self::vModul($vCA)))*180/pi(), 1);
+        return $angles;
+    }
+    function sidesLength($A, $B, $C){ //длины сторон
+        $sidesL = Array();
+    
+        $sidesL['AB'] = round(self::vModul((self::vector($A, $B))), 1);
+        $sidesL['BC'] = round(self::vModul((self::vector($B, $C))), 1);
+        $sidesL['CA'] = round(self::vModul((self::vector($C, $A))), 1);
+        return $sidesL;
+    }
+    function sProd($A, $B){ //скалярное произведение
+        return $A['x']*$B['x']+$A['y']*$B['y']+$A['z']*$B['z'];
+    }
+    function vector($A, $B){ //вектор
+        $AB = Array();
+    
+        $AB['x'] = $B['x'] - $A['x'];
+        $AB['y'] = $B['y'] - $A['y'];
+        $AB['z'] = $B['z'] - $A['z'];
+    
+        return $AB;
+    }
+    function vProd($A, $B){ //векторное произведение
+        $vProd = Array();
+    
+        $vProd['x'] = ($A['y']*$B['z'] - $A['z']*$B['y']);
+        $vProd['y'] = -($A['x']*$B['z'] - $A['z']*$B['x']);
+        $vProd['z'] = ($A['x']*$B['y'] - $A['y']*$B['x']);
+    
+        return $vProd;
+    }
+    function vModul($V){ //модуль вектора
+        return sqrt(($V['x'])*($V['x'])+($V['y'])*($V['y'])+($V['z'])*($V['z']));
+    }
+    
 }
--- server/index.php
+++ server/index.php
@@ -5,15 +5,17 @@ header('Acces-Control-Allow-Origin: *');
 
 require_once('application/Answer.php');
 require_once('application/Application.php');
+require_once('application/Triangle.php');
 
 
 function result($params) {
     $method = $params['method'];
     if ($method) {
         $app = new Application();
+        $triangle = new Triangle();
         switch ($method) {
-            // case 'triangle': return $app -> triangle($params);
             case 'login': return $app -> login($params);
+            case 'triangle': return $triangle -> triangle($params);
             // case 'method2': return $triangle -> method2($a, $b);
             // default: return array(false, 102);
         }
