4351302693be04042876f517fc032876d81848d2
Dmitry <117797846+Dmitry-Markelov@users.noreply.github.com>
2023-10-09 08:43:43 +0400

    add Routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {createBrowserRouter, Navigate, RouterProvider} from 'react-router-dom';
+import Login from './components/routes/Login';
+import Register from './components/routes/Register';
+import User from './components/routes/User';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="Login"/>,
+  },{
+    path: "/login",
+    element: <Login/>,
+  },{
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/user",
+    element: <User/>,
+  },
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <RouterProvider router={router}/>
     </div>
   );
 }
